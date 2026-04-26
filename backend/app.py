3cf8feda907d4272ec3f0097cebe3c0b8ff7e038
FrankProno <franco.peia7@gmail.com>
2026-04-26 13:22:02 +0200

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+import pandas as pd
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"message": "QuotaFootball API attiva"}
+
+@app.get("/teams")
+def get_teams():
+    df = pd.read_csv("data/teams.csv")
+    return df.to_dict(orient="records")
